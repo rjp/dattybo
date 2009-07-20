59bcdaa78773ed2b1e418ecc491dcdfaae379158
rjp <zimpenfish@gmail.com>
2009-07-20 21:25:22 +0100

    - minor code tidying - start talking to twitter

--- dattybo.pl
+++ dattybo.pl
@@ -10,9 +10,20 @@ close C;
 
 my $json = from_json($config);
 
+# TODO get this from the config file
 my $sqlite = ".dattybo.db";
 
 # cygwin appears not to like $ENV{HOME} in the dbname
 chdir $ENV{HOME};
 
+# we assume the schema is in place already because I can't work table_info
 my $dbh = DBI->connect("dbi:SQLite:db=$sqlite","","");
+
+my $twitter = Net::Twitter::Lite->new(
+    username => $config->{'username'},
+    password => $config->{'password'},
+);
+
+while ( 1 ) {
+    sleep 60;
+}
