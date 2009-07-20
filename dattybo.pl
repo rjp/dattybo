ec85335b595ea60a80c2933541c69fa648b26478
rjp <zimpenfish@gmail.com>
2009-07-20 21:02:41 +0100

    - start talking to our database file

--- dattybo.pl
+++ dattybo.pl
@@ -9,3 +9,10 @@ my $config = join('', <C>);
 close C;
 
 my $json = from_json($config);
+
+my $sqlite = ".dattybo.db";
+
+# cygwin appears not to like $ENV{HOME} in the dbname
+chdir $ENV{HOME};
+
+my $dbh = DBI->connect("dbi:SQLite:db=$sqlite","","");
