7065a6816ccd256ed19ffd7273dd554a7372e4e9
rjp <zimpenfish@gmail.com>
2009-07-21 07:27:43 +0100

    - SQL for creating our datastore - note: this is a version ahead of the code

---
+++ create.sql
@@ -0,0 +1,8 @@
+DROP TABLE datalog;
+CREATE TABLE datalog (name varchar(256), datakey varchar(256), value varchar(256), logged_at timestamp);
+DROP TABLE datatypes;
+CREATE TABLE datatypes (name varchar(256), datakey varchar(256), type char(16), primary key (name, datakey));
+DROP TABLE metadata;
+CREATE TABLE metadata (datakey char(16), value varchar(256), primary key(datakey));
+INSERT INTO metadata VALUES ('max_id', '0');
+INSERT INTO metadata VALUES ('last_boot', '1970-01-01 00:00:00');
