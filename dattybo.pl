b43a81f9889578a97cf5a1b1d3b0a45d6530c389
rjp <zimpenfish@gmail.com>
2009-07-21 07:21:44 +0100

    - again, it helps if you look in the right places

--- dattybo.pl
+++ dattybo.pl
@@ -37,9 +37,9 @@ while ( 1 ) {
     my $dm = $twitter->direct_messages();
     foreach my $i (@{$dm}) {
         # process them here if we've not seen them before
-        my $from = $dm->{'sender_screen_name'};
-        my $dmid = $dm->{'id'};
-        my $text = $dm->{'text'};
+        my $from = $i->{'sender_screen_name'};
+        my $dmid = $i->{'id'};
+        my $text = $i->{'text'};
         my ($key, $value) = split(' ', $text, 2);
     }
     sleep 60;
