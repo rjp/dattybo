d03178283b288bdb6b7ffb7e23afff8ab715eba9
Rob Partington <zimpenfish@gmail.com>
2009-09-02 16:31:47 +0100

    - rejig things - query our date range - insert the data into the hash passed to the template

--- dattybo.rb
+++ dattybo.rb
@@ -18,14 +18,15 @@ get '/*/*' do
     data_list, date = splat[1].split('/', 2)
     @days = daterange(date)
 
-    sql_range = [@days[0], @days[-1]]
+    @sql_range = [@days[0], @days[-1]]
 
     columns = []
     @data_vars = data_list.split(',')
 
     @info = Hash.new { |h,k| h[k] = {
         :type => 'counter',
-        :graph => ''
+        :graph => '',
+        :data => {}
     }}
     @data_vars.each do |d|
         type = @dbh.select_one(
@@ -33,6 +34,26 @@ get '/*/*' do
 	        username, d
         )
         type = type || 'value'
+        @info[d][:type] = type
+
+        if type[0] == 'counter' then
+            data = @dbh.select_all(
+                "SELECT DATE(logged_at) AS logged_date,
+		               COUNT(1) AS logged, MIN(value) AS min,
+		               MAX(value) AS max, AVG(value) AS avg,
+		               SUM(value) AS sum
+		        FROM datalog
+		        WHERE date(logged_at) between ? and ?
+                AND name=? AND datakey=?
+                GROUP BY DATE(logged_at)",
+                [@days[0], @days[-1], username, d]
+            )
+            @info[d][:dump] = data.inspect
+            data.each { |i|
+                dt, ct, mn, mx, av, sm = i
+                @info[d][:data][dt] = sm
+            }
+        end
     end
 
     haml :columns
