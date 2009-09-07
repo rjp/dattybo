ca44185a649fde22413fe4b8f4ea32a01e4c4b67
Rob Partington <zimpenfish@gmail.com>
2009-09-07 15:59:30 +0100

    - fix up the chart to have the correct number of days per month - remove the example URLs since we now know it was chbh - add chbh to control the bar width - add chds even though we don't strictly need it

--- dattybo.rb
+++ dattybo.rb
@@ -63,21 +63,23 @@ get '/*/*' do
             }
         end
 
-# http://chart.apis.google.com/chart?chxl=0:|2009-08-01|2009-08-11|2009-08-21|2009-08-31|1:|750|1000|1250|1500|2000&chxp=0,0.5,10.5,20.5,31.5|1,750,1000,1250,1500,2000&chxs=0,888888,9|1,888888,9,1,lt,cccccc&chs=240x100&chxt=x,y&chxtc=0,6|1,-220&chxr=1,750,2000,0|0,0,31,0&cht=bvg&chbh=a,0,0&chco=88CCFF&chds=750,2000&chd=<DATA>
-# http://chart.apis.google.com/chart?chxs=0,000000,9%7C1,000000,9&chxt=y,x&chs=240x100&chxl=1:%7C2009-09-01%7C2009-09-30&cht=bvs&chco=%2388aaff&chxp=1,0,29&chd=e:qPmf..AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA&chxr=0,750,2000%7C1,0,30
         gc = GChart.bar do |g|
             g.orientation = :vertical
             g.data = [chart_data]
             g.colors = ['#88aaff']
             g.width = 240
             g.height = 100
+# TODO make this non-hardcoded
             g.min = 750
             g.max = 2000
 
             g.axis(:left) { |a|
-                a.range = 750..2000
                 a.text_color = :black
                 a.font_size = 9
+# TODO make this non-hardcoded
+                a.range = 750..2000
+                g.extras.merge!({ :chds => '750,2000' })
+                a.label_positions = [750, 1000, 1500, 2000]
             }
 
             g.axis(:bottom) do |a|
@@ -87,6 +89,8 @@ get '/*/*' do
                 a.text_color = :black
                 a.font_size = 9
             end
+
+            g.extras.merge!({ :chbh => 'a,0,0' })
         end
 
         @info[d][:graph] = gc.to_url
